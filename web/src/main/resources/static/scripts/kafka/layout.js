4d6ee2e942f743ecd0141e317dd5916bc1abc9b7
vizit <youngok2004@gmail.com>
2020-07-26 09:44:36 +0800

    kafka cluster layout is done

--- web/src/main/resources/static/scripts/kafka/layout.js
+++ web/src/main/resources/static/scripts/kafka/layout.js
@@ -1,19 +1,28 @@
-let broker_length = 100;
-let broker_padding = 100;
-let zk_length = 100;
+let node_width = 200;
+let node_height = 100;
+let x_padding = 200;
+let y_padding = 300;
 
 export function layout(root) {
-    if (root.depth == 0) { // zk
-        root.x = 300;
+    if (root.depth == 0) {
+        root.x = 0;
         root.y = 0;
-        root.length = zk_length;
     }
-    if (root.depth == 1) { // brokers
+    if (root.depth == 1) {
         let index = root.data.index;
-        root.x = index * (broker_length + broker_padding);
-        root.y = zk_length + broker_padding;
-        root.length = broker_length;
-        root.id = index;
+        root.width = node_width;
+        root.height = node_height;
+        // zk
+        if (root.data.key == 'zk') {
+            root.x = 400;
+            root.y = 100;
+            root.id = 'zk';
+            return root;
+        }
+        // brokers
+        root.x = index * (node_width + x_padding);
+        root.y = 100 + y_padding;
+        root.id = 'broker-'+index;
     }
 
     if (root.depth > 1) { // object
--- web/src/main/resources/static/scripts/kafka/svg.js
+++ web/src/main/resources/static/scripts/kafka/svg.js
@@ -2,7 +2,7 @@ import {layout} from "./layout.js";
 
 let colorCategory10 = d3.scaleOrdinal(d3.schemeCategory10);
 let colorPastel2 = d3.scaleOrdinal(d3.schemePastel2);
-let margin = {top: 40, right: 90, bottom: 50, left: 150},
+let margin = {top: 40, right: 90, bottom: 50, left: 50},
     width = 1200 - margin.left - margin.right,
     height = 1000 - margin.top - margin.bottom;
 
@@ -16,14 +16,20 @@ let svg = d3.select("#paper")
 
 export function init() {
     let treeData = {
-        key: 625,
+        key: "kafka",
         index: 0,
         children: []
     };
+    let zk = {
+        key: "zk",
+        index: 0,
+        children: []
+    }
+    treeData.children.push(zk);
 
     for (let i = 0; i < 3; i++) {
         let broker = {
-            key: i,
+            key: "broker-" + i,
             index: i,
             children: []
         }
@@ -33,13 +39,13 @@ export function init() {
     let hierarchy = d3.hierarchy(treeData);
     let root = layout(hierarchy);
 
-    g.selectAll(".broker")
+    g.selectAll(".node")
         .data(root.decendents)
         .enter()
         .append("rect")
-        .classed('broker', true)
+        .classed('node', true)
         .attr("id", d => {
-            return 'region-' + d.id
+            return d.id
         })
         .attr("x", d => {
             return d.x
@@ -48,51 +54,33 @@ export function init() {
             return d.y;
         })
         .attr("width", d => {
-            return d.length;
+            return d.width;
         })
         .attr("height", d => {
-            return d.length;
+            return d.height;
         })
         .attr("fill", d => {
-            let i = d.id;
-            let color = colorPastel2(0);
-            if (i == 11 || i == 13) {
-                color = colorPastel2(2);
-            } else if (i == 24) {
-                color = colorPastel2(3);
-            } else if (i % 2 == 0 && i < 10) {
-                color = colorPastel2(0);
-            } else {
-                color = colorPastel2(1);
+            if (d.data.key == 'zk') {
+                return colorPastel2(0);
             }
-            return color;
+            let i = d.data.index;
+            return colorPastel2(i+1);
         })
         .attr("stroke", "blue");
 
-    g.selectAll(".region_label")
+    g.selectAll(".node_label")
         .data(root.decendents)
         .enter()
         .append("text")
         .attr("x", d => {
-            return d.x + d.length / 2;
+            return d.x + d.width / 2;
         })
         .attr("y", d => {
-            return d.y + d.length / 2;
+            return d.y + d.height / 2;
         })
-        .classed('region_label', true)
+        .classed('node_label', true)
         .style("text-anchor", "middle")
         .text(function (d) {
-            let i = d.id;
-            let text = 'E';
-            if (i == 11 || i == 13) {
-                text = 'S';
-            } else if (i == 24) {
-                text = 'H';
-            } else if (i % 2 == 0 && i < 10) {
-                text = 'E';
-            } else {
-                text = 'O';
-            }
-            return text;
+            return d.id;
         });
 }
