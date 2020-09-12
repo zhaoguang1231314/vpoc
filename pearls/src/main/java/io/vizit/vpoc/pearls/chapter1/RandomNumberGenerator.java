d473157238c86b2ac70654b4f7c180dc52148b5f
vizit <youngok2004@gmail.com>
2020-09-12 12:15:18 +0800

    add program pearls

--- pearls/src/main/java/io/vizit/vpoc/pearls/chapter1/RandomNumberGenerator.java
+++ pearls/src/main/java/io/vizit/vpoc/pearls/chapter1/RandomNumberGenerator.java
@@ -4,20 +4,23 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
 
 public class RandomNumberGenerator {
 
-    public static final int N = 10000000;
+    public static final int MIN = 1000000;
+    public static final int MAX = 9999999;
+    public static final int N = MAX - MIN + 1;
 
     public static void main(String[] args) {
-        List<Integer> integers = ThreadLocalRandom.current()
-                .ints(N, 0, N)
-                .distinct()
-                .boxed()
-                .collect(Collectors.toList());
+
+        List<Integer> integers = new ArrayList<>(N);
+        for (int i = MIN; i <= MAX; i++) {
+            integers.add(i);
+        }
+        Collections.shuffle(integers);
 
         String filepath = "./pearls/phone_numbers.txt";
         File file = new File(filepath);
